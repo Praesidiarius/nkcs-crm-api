78c39615caa8b7fa603ddb8a1899d9eed611ef23
Praesidiarius <admin@1plc.ch>
2023-06-07 17:26:16 +0200

    add more example data

---
+++ data/sql/job_position_unit_basic.sql
@@ -0,0 +1,3 @@
+INSERT INTO `job_position_unit` (`id`, `name`) VALUES
+(1, 'job.unit.piece'),
+(2, 'job.unit.hour');
---
+++ data/sql/job_type_basic.sql
@@ -0,0 +1,3 @@
+INSERT INTO `job_type` (`id`, `type_key`, `type_value`) VALUES
+(1, 'job', 'Job'),
+(2, 'offer', 'Offer');
