4a6c9974078be285fcb0d11f304473f35a0e974e
Praesidiarius <admin@1plc.ch>
2023-07-15 17:05:52 +0200

    extend and rename db build script

--- data/scripts/buildDatabase.sh
+++ data/scripts/buildDatabase.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# password hash equals "test"
-DEV_PASSWORD="\$2y\$13\$cMyLSyniGkyrM2IhCm68vejEqypYm6vGCsngOgc4VARcSeky2yAw6"
-DB_NAME=${1:-nkcs_test}
 DB_USER="root"
 DB_PASS="root"
 SQL_TEMPLATE_DIR="./../sql"
+DB_NAME=${1:-nkcs_test}
+USER_USERNAME=${2:-dev}
+# password hash equals "test"
+USER_PASSWORD=${3:-"\$2y\$13\$cMyLSyniGkyrM2IhCm68vejEqypYm6vGCsngOgc4VARcSeky2yAw6"}
+USER_FIRSTNAME=${4-"Dev"}
+USER_LASTNAME=${5-"System"}
+USER_EMAIL=${6-"dev@example.com"}
 
 # make sure sql template directory exists before running the script
 if [ ! -d "${SQL_TEMPLATE_DIR}" ]; then
@@ -17,11 +21,11 @@ mysql -u${DB_USER} -p${DB_PASS} -e "DROP DATABASE IF EXISTS ${DB_NAME}"
 mysql -u${DB_USER} -p${DB_PASS} -e "CREATE DATABASE ${DB_NAME} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 
 CREATE_USER_SQL=$(cat "${SQL_TEMPLATE_DIR}"/db_live_user_template.sql)
-CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##USERNAME##|dev|g")
-CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##PASSWORD##|${DEV_PASSWORD}|g")
-CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##FIRST##|Dev|g")
-CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##LAST##|System|g")
-CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##EMAIL##|dev@vivid-crm.io|g")
+CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##USERNAME##|${USER_USERNAME}|g")
+CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##PASSWORD##|${USER_PASSWORD}|g")
+CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##FIRST##|${USER_FIRSTNAME}|g")
+CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##LAST##|${USER_LASTNAME}|g")
+CREATE_USER_SQL=$(echo "${CREATE_USER_SQL}" | sed -r "s|##EMAIL##|${USER_EMAIL}|g")
 
 # rebuild db
 # core db schema
