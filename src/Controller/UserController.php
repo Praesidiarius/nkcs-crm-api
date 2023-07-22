e477c7c1ff136e9a12cb7b3949f4e07042981c92
Praesidiarius <admin@1plc.ch>
2023-07-22 17:41:19 +0200

    fix user controller for license server

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\SystemSettingRepository;
 use App\Service\User\ClientNavigation;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,7 @@ class UserController extends AbstractApiController
     #[Route('/')]
     public function getUserInfo(
         ClientNavigation $clientNavigation,
+        Request $request,
     ): Response {
         $me = $this->getUser();
 
@@ -31,20 +33,35 @@ class UserController extends AbstractApiController
         ];
 
         if ($this->getParameter('license.server')) {
-            $daysLeft = 0;
-            if ($this->getLicenseValidUntilDate()) {
-                if (strtotime($this->getLicenseValidUntilDate()) > time()) {
-                    $daysLeft = round(((strtotime($this->getLicenseValidUntilDate())  - time()) / 3600) / 24);
+            // make sure we are on the license server instance
+            $self = ($request->server->getBoolean('HTTPS') ? 'https://' : 'http://')
+                . $request->server->get('HTTP_HOST');
+
+            if ($this->getParameter('license.server') === $self) {
+                $userApiData['license'] = [
+                    'isTrial' => false,
+                    'daysLeft' => 300,
+                    'dateValid' => date('d.m.Y', strtotime('+300 days')),
+                    'product' => null,
+                    'future' => [],
+                    'archive' => [],
+                ];
+            } else {
+                $daysLeft = 0;
+                if ($this->getLicenseValidUntilDate()) {
+                    if (strtotime($this->getLicenseValidUntilDate()) > time()) {
+                        $daysLeft = round(((strtotime($this->getLicenseValidUntilDate())  - time()) / 3600) / 24);
+                    }
                 }
+                $userApiData['license'] = [
+                    'isTrial' => $this->isTrial(),
+                    'daysLeft' => $daysLeft,
+                    'dateValid' => $this->getLicenseValidUntilDate() ? date('d.m.Y', strtotime($this->getLicenseValidUntilDate())) : '',
+                    'product' => $this->getLicenseProduct(),
+                    'future' => $this->getFutureLicenses(),
+                    'archive' => $this->getArchivedLicenses(),
+                ];
             }
-            $userApiData['license'] = [
-                'isTrial' => $this->isTrial(),
-                'daysLeft' => $daysLeft,
-                'dateValid' => $this->getLicenseValidUntilDate() ? date('d.m.Y', strtotime($this->getLicenseValidUntilDate())) : '',
-                'product' => $this->getLicenseProduct(),
-                'future' => $this->getFutureLicenses(),
-                'archive' => $this->getArchivedLicenses(),
-            ];
         }
         return $this->json($userApiData);
     }
